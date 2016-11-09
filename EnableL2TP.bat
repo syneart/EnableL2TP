5ec92efa57df4c3730b713f82b47df2ca9ca9e54
SyneArt <syneart@users.noreply.github.com>
2016-11-09 19:52:53 +0800

    Update EnableL2TP.bat

--- EnableL2TP.bat
+++ EnableL2TP.bat
@@ -1,16 +1,29 @@
+:: Copyright by SyneArt <sa@syneart.com>
 @echo off
-echo Crate ProhibitIpSec with 0
+call :check_Permissions
+echo.&echo Create key "ProhibitIpSec" with 0
 reg add "HKLM\SYSTEM\CurrentControlSet\services\RasMan\Parameters" /v ProhibitIpSec /t REG_DWORD /d 0 /f
-echo Crate AssumeUDPEncapsulationContextOnSendRule with 2
+echo.&echo Create key "AssumeUDPEncapsulationContextOnSendRule" with 2
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\PolicyAgent" /v AssumeUDPEncapsulationContextOnSendRule /t REG_DWORD /d 2 /f
-echo Process service - Remote Access Auto Connection Manager
+echo.&echo Active service "Remote Access Auto Connection Manager"
 sc config RasAuto start= auto
 sc start RasAuto
-echo Process service - Remote Access Connection Manager
+echo.&echo Active service "Remote Access Connection Manager"
 sc config RasMan start= auto
 sc start RasMan
-echo Process service - Secure Socket Tunneling Protocol Service
+echo.&echo Active service "Secure Socket Tunneling Protocol Service"
 sc config SstpSvc start= auto
 sc start SstpSvc
 echo All finish, please REBOOT your system.
-pause
+pause >nul
+
+:check_Permissions
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate. 
+		echo Administrative permissions required.
+		pause >nul
+		EXIT
+    )
